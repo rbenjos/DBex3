b9c97e3f02c808bd2812acf11533f29313050a16
rbenjos <roey.benyosef@mail.huji.ac.il>
2020-11-29 14:27:59 +0200

    done with q5

---
+++ q10.sql
@@ -0,0 +1,4 @@
+EXPLAIN ANALYZE
+SELECT 'exists'
+FROM Movies
+WHERE duration > 100
--- q5.sql
+++ q5.sql
@@ -1,19 +1,16 @@
 WITH RECURSIVE T AS
 (
-SELECT actorId, movieId, 0 as bacon
+SELECT actorId, 0 as bacon
 FROM Actors NATURAL JOIN playsIn
 WHERE name = 'Frank Bacon'
-UNION ALL
-
-SELECT p1.actorId, p2.movieId, bacon+1
-FROM T INNER JOIN PlaysIn p1 ON (p1.movieId = T.movieId AND T.bacon < 6) INNER JOIN PlaysIn p2 ON (p1.actorId = p2.actorID)
-
+UNION
+SELECT actorId , bacon+1
+FROM PlaysIn NATURAL JOIN
+(
+SELECT movieId, bacon
+FROM T NATURAL JOIN PlaysIn
+) M2
+WHERE bacon < 5
 )
-SELECT actorId
-FROM actors
-WHERE actorId IN 
-(SELECT actorId
-	FROM T
-	GROUP BY actorID
-	WHERE bacon < 6)
-;
+
+SELECT DISTINCT * FROM T;
